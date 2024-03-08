5cacb8ba8d00ed238853f8a986862370ab203077
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-03-08 13:21:48 -0300

    feat: add redux provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import {Provider as ReduxProvider} from 'react-redux'
+import { AddTodo } from "./components/Addtodo";
+import { TodoList } from "./components/TodoList";
+import { store } from './store';
 
 
 export function App() {
 
 
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+    <ReduxProvider store={store}>
+      <div>
+        <h1>Hello</h1>
+        <TodoList/>
+        <AddTodo/>
+      </div>
+    </ReduxProvider>
   )
 }
 
