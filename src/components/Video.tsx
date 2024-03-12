68182de89f8c7bd8f4e2301abedf8330ccfa8575
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-03-12 15:27:01 -0300

    feat: hook for global infos

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,18 +1,11 @@
 import ReactPlayer from 'react-player'
-import { useAppSelector } from '../store';
+
 import { useDispatch } from "react-redux";
-import { next } from "../store/slices/player";
+import { next, useCurrentLesson } from "../store/slices/player";
 
 export function Video() {
+  const { currentLesson } = useCurrentLesson()
   const dispatch = useDispatch()
-  const lesson = useAppSelector(state => {
-    const { currentModuleIndex, currentLessonIndex } = state.player
-
-    const currentLesson =
-      state.player.course.modules[currentModuleIndex].lessons[currentLessonIndex]
-
-    return currentLesson
-  })
 
   function handlePlayNext() {
     dispatch(next())
@@ -25,7 +18,7 @@ export function Video() {
         controls
         playing
         onEnded={handlePlayNext}
-        url={`https://www.youtube.com/watch?v=${lesson.id}`}
+        url={`https://www.youtube.com/watch?v=${currentLesson.id}`}
       />
     </div>
   );
