33517739fe530c86dd782201f26a82fc1c9b61ed
Gabriel Gomes <gabrielgomes98@gmail.com>
2024-03-14 12:20:09 -0300

    refactor: test store

--- src/zustand-store/store.spec.ts
+++ src/zustand-store/store.spec.ts
@@ -43,46 +43,32 @@ describe('zustand store', () => {
 
   it('should be able to play next video automatically', () => {
     store.setState({ course })
-
     const { next } = store.getState()
-
     next()
-
     const { currentModuleIndex, currentLessonIndex } = store.getState()
-
     expect(currentModuleIndex).toEqual(0)
     expect(currentLessonIndex).toEqual(1)
   })
 
   it('should be able to jump to the next module automatically', () => {
     store.setState({ course })
-
     const { next } = store.getState()
-
     store.setState({ currentLessonIndex: 1 })
-
     next()
-
     const { currentModuleIndex, currentLessonIndex } = store.getState()
-
     expect(currentModuleIndex).toEqual(1)
     expect(currentLessonIndex).toEqual(0)
   })
 
   it('should not update the current module and lesson index if there is not next lesson available', () => {
     store.setState({ course })
-
     const { next } = store.getState()
-
     store.setState({
       currentModuleIndex: 1,
       currentLessonIndex: 1
     })
-
     next()
-
     const { currentModuleIndex, currentLessonIndex } = store.getState()
-
     expect(currentModuleIndex).toEqual(1)
     expect(currentLessonIndex).toEqual(1)
   })
